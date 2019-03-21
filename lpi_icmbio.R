22daf8e51c013b78c286b77a5d294ada7ddd1fae
Elildo Carvalho Jr <39089964+elildojr@users.noreply.github.com>
2019-03-21 13:50:05 -0300

    Update lpi_icmbio.R

--- lpi_icmbio.R
+++ lpi_icmbio.R
@@ -15,16 +15,16 @@ dados2 <- dados[dados$Espécies.validadas.para.análise.do.ICMBio != "",] # remo
 
 # Função lpi_icmbio:
 lpi_icmbio <- function(x,y,z) # x = dados, y = UC, z = Classe
-  {
+{
   library(rlpi) # carrega pacote rlpi
   
   mydata <- x
   
-    if(missing(y)) {    mydata <- mydata }
-    else {  mydata <- subset(x, Local...Nome.da.Unidade.de.Conservação == y) } # seleciona UC
+  if(missing(y)) {    mydata <- mydata }
+  else {  mydata <- subset(x, Local...Nome.da.Unidade.de.Conservação == y) } # seleciona UC
   
-    if(missing(z)) {    mydata <- mydata }
-    else {  mydata <- subset(mydata, Classe == z) } # seleciona taxon
+  if(missing(z)) {    mydata <- mydata }
+  else {  mydata <- subset(mydata, Classe == z) } # seleciona taxon
   
   colnames(mydata)[20] <- "Binomial"
   mydata$Binomial <- gsub(" ", "_", mydata$Binomial) # necessário para rodar LPIMain
@@ -34,33 +34,37 @@ lpi_icmbio <- function(x,y,z) # x = dados, y = UC, z = Classe
   # colocar dados no formato exigido pela função LPIMain
   # passo 1, criar objeto a ser preenchido
   mydata2 <- data.frame(matrix(ncol = (2+length(unique(mydata$Ano))), nrow = length(unique(mydata$Binomial))))
-  colnames(mydata2) <- c("ID", "Binomial","X2014","X2015","X2016","X2017") # Substituir futuramente por função que cria automaticamente os nomes de colunas de anos
+  colnames(mydata2) <- c("ID", "Binomial", sort(unique(mydata$Ano))) # cria automaticamente os nomes de colunas de anos
   mydata2$ID <- c(1:nrow(mydata2))
   mydata2$Binomial <- sort(unique(mydata$Binomial))
+  Ano <- sort(unique(mydata$Ano)) # sera necessario abaixo
   
   # passo 2, preencher objeto criado acima
-  for(i in 1:nrow(mydata2)) {
+  for(i in 1:nrow(mydata2))
+  for(j in 1:length(Ano)){
     a <- subset(mydata, Binomial == mydata2[i,2])
-    ano1 <- subset(a, Ano == 2014) # Substituir futuramente por função que extrai o ano automaticamente
-    ano2 <- subset(a, Ano == 2015)
-    ano3 <- subset(a, Ano == 2016)
-    ano4 <- subset(a, Ano == 2017)
-    mydata2[i,3] <-  nrow(ano1)/(sum(subset(mydata, Ano == 2014)$Dias.km.de.amostragem..esforço., na.rm=TRUE)/1000/10)
-    mydata2[i,4] <-  nrow(ano2)/(sum(subset(mydata, Ano == 2015)$Dias.km.de.amostragem..esforço., na.rm=TRUE)/1000/10)
-    mydata2[i,5] <-  nrow(ano3)/(sum(subset(mydata, Ano == 2016)$Dias.km.de.amostragem..esforço., na.rm=TRUE)/1000/10)
-    mydata2[i,6] <-  nrow(ano4)/(sum(subset(mydata, Ano == 2017)$Dias.km.de.amostragem..esforço., na.rm=TRUE)/1000/10)
+    b <- subset(a, Ano == Ano[j]) # extrai o ano automaticamente
+    #mydata2[i,j+2] <- nrow(b)/(sum(mydata$km.dia.de.amostragem..esforço., na.rm=TRUE))
+    mydata2[i,j+2] <- nrow(b)/(sum(subset(mydata, Ano == Ano[j])$km.dia.de.amostragem..esforço., na.rm=TRUE))*10000 # o esforco usa a planilha mydata porque so uma linha por censo tem esforco
     }
-
-  mydata3 <<- mydata2 # salva no workspace para conferir se está ok
   
+  #mydata2[, colSums(is.na(mydata2)) != nrow(mydata2)] # remover coluna de NAs se houver
+  require(stringr) # necessario para passo abaixo
+  colnames(mydata2)[3:ncol(mydata2)] <- str_c( "X", colnames(mydata2)[3:ncol(mydata2)]) # adiciona um "X" no nome das colunas de anos porque o create_infile exige
+  assign("taxas.medias", mydata2, envir=globalenv())
+  assign("mydata2", mydata2, envir=globalenv())
+
   # criar vetor indice com TRUE para todas as linhas, pois todas as espécies serão incluídas
   index_vector = rep(TRUE, nrow(mydata2))
   
   # criar infile
-  mydata_infile <- create_infile(mydata2, index_vector=index_vector, name="mydata_data", start_col_name = "X2014", end_col_name = "X2017", CUT_OFF_YEAR = 2014)
+  mydata_infile <- create_infile(mydata2, index_vector=index_vector, name="mydata_data", start_col_name = colnames(mydata2)[3], end_col_name = tail(colnames(mydata2), n=1), CUT_OFF_YEAR = Ano[1])
+  #mydata_infile <- create_infile(mydata2, index_vector=index_vector, name="mydata_data", start_col_name = "X2014", end_col_name = "X2016", CUT_OFF_YEAR = 2014)
+  #mydata_infile <- create_infile(mydata2, index_vector=index_vector, name="mydata_data", start_col_name = "2014", end_col_name = "2017", CUT_OFF_YEAR = "2014")
+  
   
   # Calcular LPI com 100 bootstraps
-  mydata_lpi <- LPIMain(mydata_infile, REF_YEAR = 2014, PLOT_MAX = 2017, BOOT_STRAP_SIZE = 100, VERBOSE=FALSE)
+  mydata_lpi <- LPIMain(mydata_infile, REF_YEAR = Ano[1], PLOT_MAX = tail(Ano, n=1), BOOT_STRAP_SIZE = 100, VERBOSE=FALSE)
   
   # Remover NAs (anos seguidos sem dados)
   mydata_lpi <- mydata_lpi[complete.cases(mydata_lpi), ]
@@ -72,7 +76,6 @@ lpi_icmbio <- function(x,y,z) # x = dados, y = UC, z = Classe
   ggplot_lpi_modif(mydata_lpi, col="cornflowerblue")
   
 } # Fim da função
-
 
 # Testando a função
 
